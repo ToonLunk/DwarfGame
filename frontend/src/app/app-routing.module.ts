56b9dd0f4da372d0850205b9a3335b49d7e50ba9
Toon Lunk <tylerchasebrock@gmail.com>
2024-01-29 15:20:23 -0600

    Add landing page and header components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: '**', redirectTo: 'landing-page' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
